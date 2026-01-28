cb19c2fe0a5b0a58aee31eae2dfdfc1ae99b49fb
Ak62007 <ied10007.23@bitmesra.ac.in>
2026-01-28 19:21:23 +0000

    now this can generate a whole website, containing multiple webpages from a dir of markdown files.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import re
 import os
+from pathlib import Path
 import shutil
 from blocks import (
     block_to_block_type,
@@ -49,7 +50,41 @@ def extract_title(markdown: str) -> str:
                 return block[result.end():]
                 
     raise ValueError("Markdown must have an h1 title")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    
+    for file_name in os.listdir(dir_path_content):
+        file_path = os.path.join(dir_path_content, file_name)
+        if os.path.isfile(file_path):
             
+            if not file_name.endswith(".md"):
+                continue
+            
+            with open(file_path, 'r') as f:
+                md_contents = f.read()
+            
+            with open(template_path, 'r') as f:
+                templete = f.read()
+                
+            node = markdown_to_html_node(markdown=md_contents)
+            html_str = node.to_html()
+            title = extract_title(markdown=md_contents)
+            
+            # Replacing Title and Content
+            templete = templete.replace("{{ Title }}", title)
+            templete = templete.replace("{{ Content }}", html_str)
+            
+            # Changing the extensions
+            html_file_name = file_name.replace(".md", ".html")
+            target_file_path = os.path.join(dest_dir_path, html_file_name)
+            
+            os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
+            
+            with open(target_file_path, 'w') as f:
+                f.write(templete)
+        else:
+            new_target_dir = os.path.join(dest_dir_path, file_name)
+            generate_pages_recursive(file_path, template_path, new_target_dir)
 
 def transfer_contents(source: str = None, dest: str = None, first_call: bool = True):
     dest_abs = os.path.abspath(dest)
@@ -75,10 +110,10 @@ def main():
     transfer_contents(dest="public", source="static")
     
     # Generating from content/index.md using template.html and write it to public/index.html.
-    generate_page(
-        from_path="content/index.md",
+    generate_pages_recursive(
+        dir_path_content="content",
         template_path="template.html",
-        dest_path="public/index.html"
+        dest_dir_path="public"
     )
     
 if __name__ == "__main__":
